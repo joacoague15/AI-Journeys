0c2d95501dd62088c29105bac515ea2ed94368f0
joacoague15 <joacoague15@gmail.com>
2023-03-03 16:37:33 +0000

    Added first functional version of input output

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { useState } from "react";
+
 function App() {
+    const [currentPrompt, setCurrentPrompt] = useState("");
+    const [prompts, setPrompts] = useState([]); // This is where we will store all the prompts
+    const [chatGPTresponses, setChatGPTresponses] = useState([]); // This is where we will store all the chatGPT responses
+
+    const submitPrompt = e => {
+        if (e.key === "Enter") {
+            setPrompts([...prompts, currentPrompt]);
+            setChatGPTresponses([...chatGPTresponses, "This is some random response from GPT-3"]);
+            setCurrentPrompt("");
+        }
+    }
+
   return (
-    <div className="App">
-      Hi
+    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", textAlign: "center", minHeight: "100vh" }} className="App">
+        <textarea value={currentPrompt} onKeyDown={submitPrompt} onChange={event => setCurrentPrompt(event.target.value)} style={{ width: "50%", color: "white", fontSize: 18 }} aria-label="With textarea"></textarea>
+
+        {prompts.map((prompt, index) => {
+            return <div style={{ width: "50%", color: "white", textAlign: "left", marginTop: 10, fontSize: 18 }}>
+                <p>{prompt}</p>
+                <p style={{ backgroundColor: "grey", borderRadius: 1 }}>{chatGPTresponses[index]}</p>
+            </div>
+        })}
     </div>
   );
 }
