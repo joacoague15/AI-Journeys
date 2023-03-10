7a0dfb78a40a44401261b6f0525447f914a9bb26
joacoague15 <joacoague15@gmail.com>
2023-03-10 19:03:38 +0000

    Added animation to class selection

--- src/character_creation/ClassSelection.js
+++ src/character_creation/ClassSelection.js
@@ -1,10 +1,21 @@
 const ClassSelection = ({userName, setUserClass}) => {
     return (
         <div>
-            <h2 style={{ fontSize: 32, color: "white" }}>Character class of {userName}</h2>
-            <button style={{ margin: 20, fontSize: 48 }} onClick={() => setUserClass('warrior')} type="button" className="btn btn-light">Warrior</button>
-            <button style={{ margin: 20, fontSize: 48 }} onClick={() => setUserClass('archer')} type="button" className="btn btn-light">Archer</button>
-            <button style={{ margin: 20, fontSize: 48 }} onClick={() => setUserClass('mage')} type="button" className="btn btn-light">Mage</button>
+            <h2 style={{ fontSize: 48, color: "white", marginBottom: 50 }}>Class of {userName}</h2>
+            <div style={{ display: "flex", flexDirection: "row" }}>
+                <div style={{ display: "flex", flexDirection: "column", margin: 30 }}>
+                    <button style={{ margin: 20, fontSize: 48, backgroundColor: "black", border: "none" }} onClick={() => setUserClass('warrior')} type="button" className="btn btn-light"><i style={{ color: "white", fontSize: 240, animationDuration: "4s" }} className="fa-solid fa-khanda fa-shake"></i></button>
+                    <h3 style={{ fontSize: 32, color: "white", marginBottom: 5, letterSpacing: 2 }}>Warrior</h3>
+                </div>
+                <div style={{ display: "flex", flexDirection: "column", margin: 30 }}>
+                    <button style={{ margin: 20, fontSize: 48, backgroundColor: "black", border: "none" }} onClick={() => setUserClass('archer')} type="button" className="btn btn-light"><i style={{ color: "white", fontSize: 240, animationDuration: "5s" }} className="fa-solid fa-hat-wizard fa-fade"></i></button>
+                    <h3 style={{ fontSize: 32, color: "white", marginBottom: 5, letterSpacing: 2 }}>Mage</h3>
+                </div>
+                <div style={{ display: "flex", flexDirection: "column", margin: 30 }}>
+                    <button style={{ margin: 20, fontSize: 48, backgroundColor: "black", border: "none" }} onClick={() => setUserClass('mage')} type="button" className="btn btn-light"><i style={{ color: "white", fontSize: 240, animationDuration: "4s" }} className="fa-solid fa-bullseye fa-flip"></i></button>
+                    <h3 style={{ fontSize: 32, color: "white", marginBottom: 5, letterSpacing: 2 }}>Archer</h3>
+                </div>
+            </div>
         </div>
     )
 }
