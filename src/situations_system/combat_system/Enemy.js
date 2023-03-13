cb4d23cec30223313c8c1d82ff8c1c4a8a8d6f60
joacoague15 <joacoague15@gmail.com>
2023-03-13 20:40:32 +0000

    Added black and white filter to images

--- src/situations_system/combat_system/Enemy.js
+++ src/situations_system/combat_system/Enemy.js
@@ -20,7 +20,7 @@ const Enemy = ({enemyHealth, enemyActualHealth, turn, setTurn, characterStatuses
                      aria-valuenow={enemyActualHealth} aria-valuemin="0" aria-valuemax={enemyHealth}>
                 </div>
             </div>
-            <img src={enemy} alt="enemy" style={{ maxWidth: '450px', left: '50%', position: 'absolute', transform: 'translate(-50%)', zIndex: 1 }} />
+            <img src={enemy} alt="enemy" style={{ maxWidth: '450px', left: '50%', position: 'absolute', transform: 'translate(-50%)', zIndex: 1, filter: "grayscale(100%)" }} />
         </>
     )
 }
