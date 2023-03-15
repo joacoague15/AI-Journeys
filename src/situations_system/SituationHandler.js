969f4389e82426f45630ccf77a7295e74f92f3cb
Mauricio Hunau <maurih@live.com>
2023-03-14 23:12:52 -0300

    cambios de UX

--- src/situations_system/SituationHandler.js
+++ src/situations_system/SituationHandler.js
@@ -1,9 +1,12 @@
 import CombatHandler from "./combat_system/CombatHandler";
 import Walking from "./Walking";
+//import Loot from "./Loot"
 
-const SituationHandler = ({situation, setSituation, characterStatuses, setCharacterStatuses, experience, setExperience}) => {
+const SituationHandler = ({ situation, setSituation, characterStatuses, setCharacterStatuses, experience, setExperience }) => {
 
-    if (situation === 'walking') return <Walking setSituation={setSituation}/>
+    if (situation === 'walking') return <Walking setSituation={setSituation} />
+
+    //if (situation === 'loot') return <Loot setSituation={setSituation} />
 
     if (situation === 'combat') return <CombatHandler characterStatuses={characterStatuses} setCharacterStatuses={setCharacterStatuses} setSituation={setSituation} experience={experience} setExperience={setExperience} />
 }
--- src/situations_system/combat_system/Enemy.js
+++ src/situations_system/combat_system/Enemy.js
@@ -1,26 +1,26 @@
 import enemy from "../../images/enemy1.jpg";
-import {useEffect} from "react";
+import { useEffect } from "react";
 
-const Enemy = ({enemyHealth, enemyActualHealth, turn, setTurn, characterStatuses, setCharacterStatuses}) => {
+const Enemy = ({ enemyHealth, enemyActualHealth, turn, setTurn, characterStatuses, setCharacterStatuses }) => {
 
     useEffect(() => {
         if (turn === 'enemy' && enemyActualHealth > 0) {
             setTimeout(() => {
-                    setCharacterStatuses({...characterStatuses, health: characterStatuses.health - 1});
-                    setTurn('player');
+                setCharacterStatuses({ ...characterStatuses, health: characterStatuses.health - 1 });
+                setTurn('player');
             }
-            , 1000);
+                , 1000);
         }
     }, [turn]);
 
     return (
         <>
-            <div className="progress" style={{ backgroundColor: "black", width: "50%", margin: "auto", position: "absolute",  left: "50%", transform: 'translate(-50%)', bottom: '30%' }}>
-                <div style={{ width: ((enemyActualHealth * 100)  / enemyHealth) + '%', backgroundColor: "white" }} className="progress-bar progress-bar-striped" role="progressbar"
-                     aria-valuenow={enemyActualHealth} aria-valuemin="0" aria-valuemax={enemyHealth}>
+            <div className="progress" style={{ backgroundColor: "black", width: "50%", margin: "auto", position: "absolute", left: "50%", transform: 'translate(-50%)', bottom: '20%', zIndex: 10 }}>
+                <div style={{ width: ((enemyActualHealth * 100) / enemyHealth) + '%', backgroundColor: "white" }} className="progress-bar progress-bar-striped" role="progressbar"
+                    aria-valuenow={enemyActualHealth} aria-valuemin="0" aria-valuemax={enemyHealth}>
                 </div>
             </div>
-            <img src={enemy} alt="enemy" style={{ maxWidth: '450px', left: '50%', position: 'absolute', transform: 'translate(-50%)', zIndex: 1, filter: "grayscale(100%)" }} />
+            <img src={enemy} alt="enemy" style={{ maxWidth: '65%', left: '50%', position: 'absolute', transform: 'translate(-50%)', zIndex: 1, filter: "grayscale(100%)" }} />
         </>
     )
 }
