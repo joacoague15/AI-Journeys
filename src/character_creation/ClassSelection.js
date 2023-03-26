9f54b4daa412247395c28886804d31a2ca6c3a39
joacoague15 <joacoague15@gmail.com>
2023-03-26 21:40:55 +0100

    Animated images for class selection

--- src/character_creation/ClassSelection.js
+++ src/character_creation/ClassSelection.js
@@ -1,5 +1,8 @@
 //import { IDBTransactionUpdateCharacter } from "../IndexedDB/CRUD"
 
+import warrior from '../images/warrior.gif';
+import mage from '../images/mage.gif';
+
 const ClassSelection = ({ userName, setUserClass }) => {
 
     /*const ClassChoice = (classChoice) => {
@@ -12,11 +15,11 @@ const ClassSelection = ({ userName, setUserClass }) => {
             <h2 style={{ fontSize: 48, color: "white", marginBottom: 50 }}>Class of {userName}</h2>
             <div style={{ display: "flex", flexDirection: "row" }}>
                 <div style={{ display: "flex", flexDirection: "column", margin: 30 }}>
-                    <button style={{ margin: 20, fontSize: 48, backgroundColor: "black", border: "none" }} onClick={() => setUserClass('warrior')} type="button" className="btn btn-light"><i style={{ color: "white", fontSize: 240, animationDuration: "4s" }} className="fa-solid fa-khanda fa-shake"></i></button>
+                    <button style={{ margin: 20, fontSize: 48, backgroundColor: "black", border: "none" }} onClick={() => setUserClass('warrior')} type="button" className="btn btn-light"><img style={{ filter: "grayscale(100%)" }} src={warrior} alt="warrior"/></button>
                     <h3 style={{ fontSize: 32, color: "white", marginBottom: 5, letterSpacing: 2 }}>Warrior</h3>
                 </div>
                 <div style={{ display: "flex", flexDirection: "column", margin: 30 }}>
-                    <button style={{ margin: 20, fontSize: 48, backgroundColor: "black", border: "none" }} onClick={() => setUserClass('archer')} type="button" className="btn btn-light"><i style={{ color: "white", fontSize: 240, animationDuration: "5s" }} className="fa-solid fa-hat-wizard fa-fade"></i></button>
+                    <button style={{ margin: 20, fontSize: 48, backgroundColor: "black", border: "none" }} onClick={() => setUserClass('archer')} type="button" className="btn btn-light"><img style={{ filter: "grayscale(100%)" }} alt="mage" src={mage}/></button>
                     <h3 style={{ fontSize: 32, color: "white", marginBottom: 5, letterSpacing: 2 }}>Mage</h3>
                 </div>
             </div>
