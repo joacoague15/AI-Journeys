ea0ff8b8204f91ac2d0f849a0a5087f34eb35549
joacoague15 <joacoague15@gmail.com>
2023-04-05 15:12:30 +0100

    Small fix

--- src/situations_system/combat_system/Enemy.js
+++ src/situations_system/combat_system/Enemy.js
@@ -28,7 +28,7 @@ const Enemy = ({ enemyCurrentHealth, enemyTotalHealth, turn, setTurn, characterS
     return (
         <>
             <h2 style={{ color: "white", fontSize: 24, textAlign: "center", marginBottom: 10, letterSpacing: 4 }}>{enemyName}</h2>
-            <img className={`animate__animated ${handleAnimation()}`} src={Enemies.lowLevel[0].image} alt="enemy" style={{ maxWidth: '600px', display: "flex", textAlign: "center", margin: "auto", filter: "grayscale(100%)" }} />
+            <img className={`animate__animated ${handleAnimation()}`} src={enemyImage} alt="enemy" style={{ maxWidth: '600px', display: "flex", textAlign: "center", margin: "auto", filter: "grayscale(100%)" }} />
             <div className="progress" style={{ backgroundColor: "black", width: "600px", margin: "auto", display: "flex" }}>
                 <div style={{ width: ((enemyCurrentHealth * 100) / enemyTotalHealth) + '%', backgroundColor: "white" }} className="progress-bar progress-bar-striped" role="progressbar"
                     aria-valuenow={enemyCurrentHealth} aria-valuemin="0" aria-valuemax={enemyTotalHealth}>
