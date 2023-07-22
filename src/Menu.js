3ece0a14d945f174af666a0359f6b928b50b6c45
joaquinaguero <joacoague15@gmail.com>
2023-07-22 18:10:15 -0300

    Menu for mobile and small screens

--- src/Menu.js
+++ src/Menu.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import { IDBDeleteDB, IDBTransactionGetAll } from "./IndexedDB/CRUD"
 import { hover, sound } from "./constants";
 import 'animate.css';
